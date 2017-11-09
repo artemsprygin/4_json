c5caf946f14e879b472a687dbae9af22be2590c3
artemsprygin <artemsprygin@me.com>
2017-11-09 18:35:16 +0300

    add json parser

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as handle:
+        raw_json = json.load(handle)
+    return raw_json
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, indent=4)
 
 
 if __name__ == '__main__':
-    pass
+    raw_json = load_data(sys.argv[1])
+    print(pretty_print_json(raw_json))
