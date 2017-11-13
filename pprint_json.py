3c1e4d55b4e56eb4f1cce4c9c8a10a02701f595a
artemsprygin <artemsprygin@me.com>
2017-11-13 18:05:57 +0300

    PEP8 fix

--- pprint_json.py
+++ pprint_json.py
@@ -8,11 +8,11 @@ def load_data(filepath):
     return in_json
 
 
-def formatter_json(json_data):
-    return json.dumps(json_data, indent=4,ensure_ascii=False)
+def json_format(json_data):
+    return json.dumps(json_data, indent=4, ensure_ascii=False)
 
 
 if __name__ == '__main__':
     in_json = load_data(sys.argv[1])
-    out_json = formatter_json(in_json)
+    out_json = json_format(in_json)
     print (out_json)
